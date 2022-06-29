0de8ca2ee6c5e3626ba1bef4977b738a8b90987b
BilgeKaanpc <kashikoi.dev@gmail.com>
2022-06-29 11:05:20 +0300

    11:05 / 29.06.2022

---
+++ lib/widgets/ortalama_hesapla_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:not_hesaplama/constants/app_constants.dart';
+class OrtalamaHesaplaPage extends StatefulWidget {
+  const OrtalamaHesaplaPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrtalamaHesaplaPage> createState() => _OrtalamaHesaplaPageState();
+}
+
+class _OrtalamaHesaplaPageState extends State<OrtalamaHesaplaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Center(
+          child: Text(Sabitler.baslikText,style: Sabitler.baslikStyle,),
+        ),
+      ),
+      body: Center(
+        child: Text("selam"),
+      ),
+    );
+  }
+}
